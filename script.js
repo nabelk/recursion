f740912fac4cb23948eb88755bc4ae0ba3288cde
Nabil Khalid <holanabilkhalid@gmail.com>
2023-04-02 22:04:46 +0800

    Create a recursive merge sort

--- script.js
+++ script.js
@@ -30,3 +30,42 @@ function fibsRec(input, arr = [0, 1]) {
 }
 
 console.log(fibsRec(8));
+
+// Recursice merge sort
+
+function mergeTwoArray(leftarr, rightarr) {
+    console.log('enter');
+    const resultArr = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < leftarr.length && rightIndex < rightarr.length) {
+        if (leftarr[leftIndex] < rightarr[rightIndex]) {
+            resultArr.push(leftarr[leftIndex]);
+            leftIndex++;
+        } else {
+            resultArr.push(rightarr[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    if (leftarr[leftIndex]) {
+        const getOtherEle = leftarr.slice(leftIndex);
+        resultArr.push(...getOtherEle);
+    } else {
+        const getOtherEle = rightarr.slice(rightIndex);
+        resultArr.push(...getOtherEle);
+    }
+
+    return resultArr;
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    const findCenter = Math.floor(arr.length / 2);
+    const leftSide = arr.slice(0, findCenter);
+    const rightSide = arr.slice(findCenter);
+
+    return mergeTwoArray(mergeSort(leftSide), mergeSort(rightSide));
+}
+console.log(mergeSort([7, 2, 4, 1, 3, 5, 6, 8, 9, 10]));
